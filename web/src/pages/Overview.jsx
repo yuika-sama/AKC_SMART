9de9a22f965f009855b3c27899566282cb3a848e
Yuika <nguyenanhduc2938@gmail.com>
2024-12-04 03:51:54 +0700

    update chart for overview

--- web/src/pages/Overview.jsx
+++ web/src/pages/Overview.jsx
@@ -7,9 +7,19 @@ import DateCard from "../containers/DateCard.jsx";
 import LeaveRequestCard from "../containers/LeaveRequestCard.jsx";
 import TimeCard from "../containers/TimeCard.jsx";
 import WorkingStateCard from "../containers/WorkingStateCard.jsx";
+import ChartCard from "../containers/ChartCard.jsx";
 import './css/Overview.css';
 
 const Overview = () => {
+  const data = [
+    { day: 'Thứ hai', hours: 6 },
+    { day: 'Thứ ba', hours: 8 },
+    { day: 'Thứ tư', hours: 10 },
+    { day: 'Thứ năm', hours: 7 },
+    { day: 'Thứ sáu', hours: 9 },
+    { day: 'Thứ bảy', hours: 2 },
+    { day: 'Chủ nhật', hours: 1 },
+  ];
   return (
     <Layout>
       <HeaderBar userName="Yuika"/>
@@ -23,6 +33,7 @@ const Overview = () => {
             </tr>
             <tr className="second-row">
               <th><WorkingStateCard /></th>
+              <th><ChartCard data = {data}  weekStart="Thứ hai"/></th>
             </tr>
           </table>
       </div>
